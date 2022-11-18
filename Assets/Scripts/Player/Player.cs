f3673ff4cf0d54f66ac925e3146747ecedbb9eff
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-11-18 14:13:43 +0900

    プレイヤー転がす挙動完成

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float cubeSizeHalf;
+    private Vector3 rotatePoint;
+    private Vector3 rotateAxis;
+    private float cubeAngle;
+    private bool isRotate;
+    public float addRotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeSizeHalf = transform.localScale.x / 2f;
+        isRotate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRotate)
+            return;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotatePoint = transform.position + new Vector3(cubeSizeHalf, -cubeSizeHalf, 0f);
+            rotateAxis = new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotatePoint = transform.position + new Vector3(-cubeSizeHalf, -cubeSizeHalf, 0f);
+            rotateAxis = new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, cubeSizeHalf);
+            rotateAxis = new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, -cubeSizeHalf);
+            rotateAxis = new Vector3(-1, 0, 0);
+        }
+
+
+        if (rotatePoint == Vector3.zero)
+            return;
+        StartCoroutine(MoveCube());
+
+    }
+
+    IEnumerator MoveCube()
+    {
+        //âÒì]íÜÇÃÉtÉâÉOÇóßÇƒÇÈ
+        isRotate = true;
+
+        //âÒì]èàóù
+        float sumAngle = 0f; //angleÇÃçáåvÇï€ë∂
+        while (sumAngle < 90f)
+        {
+            cubeAngle = addRotate; //Ç±Ç±ÇïœÇ¶ÇÈÇ∆âÒì]ë¨ìxÇ™ïœÇÌÇÈ
+            sumAngle += cubeAngle;
+
+            // 90ìxà»è„âÒì]ÇµÇ»Ç¢ÇÊÇ§Ç…ílÇêßå¿
+            if (sumAngle > 90f)
+            {
+                cubeAngle -= sumAngle - 90f;
+            }
+            transform.RotateAround(rotatePoint, rotateAxis, cubeAngle);
+
+            yield return null;
+        }
+
+        //âÒì]íÜÇÃÉtÉâÉOÇì|Ç∑
+        isRotate = false;
+        rotatePoint = Vector3.zero;
+        rotateAxis = Vector3.zero;
+
+        yield break;
+    }
+}
