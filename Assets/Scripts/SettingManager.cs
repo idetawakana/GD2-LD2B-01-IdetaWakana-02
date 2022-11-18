10a2351b1352e3a9cfa2f52730e84a3f0b85c849
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-11-18 13:30:43 +0900

    フレームレートの設定など

--- Assets/Scripts/SettingManager.cs
+++ Assets/Scripts/SettingManager.cs
@@ -7,12 +7,28 @@ public class SettingManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.SetResolution(1366, 768, false);
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EndGame();
+    }
+
+    private void EndGame()
+    {
+        //Escが押された時
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+        }
+
     }
 }
