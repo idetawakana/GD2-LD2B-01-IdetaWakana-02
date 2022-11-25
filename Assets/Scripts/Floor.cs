691ed14aece36e92441cb3455d15b52212aab4bd
WakanaIdeta <k021g1162@g.neec.ac.jp>
2022-11-25 14:24:19 +0900

    上下左右の色の取得完成

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -4,16 +4,57 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
-    
+    public float[,] floor = new float[7, 7]
+    {
+        {0,0,0,0,0,0,0},
+        {0,0,0,3,0,0,0},
+        {0,0,0,4,0,0,0},
+        {0,4,1,4,3,2,0},
+        {0,0,0,2,0,0,0},
+        {0,0,0,1,0,0,0},
+        {0,0,0,0,0,0,0}
+    };
+
+    public GameObject floorRed;
+    public GameObject floorBlue;
+    public GameObject floorYellow;
+    public GameObject floorWhite;
+
+    public Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                pos = new Vector3(i, -1, j);
+                if (floor[i, j] == 1)
+                {
+                    Instantiate(floorRed, pos, Quaternion.identity);
+                }
+
+                if (floor[i, j] == 2)
+                {
+                    Instantiate(floorBlue, pos, Quaternion.identity);
+                }
+
+                if (floor[i, j] == 3)
+                {
+                    Instantiate(floorYellow, pos, Quaternion.identity);
+                }
+
+                if (floor[i, j] == 4)
+                {
+                    Instantiate(floorWhite, pos, Quaternion.identity);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Player/PlayerColor.cs
+++ Assets/Scripts/Player/PlayerColor.cs
@@ -1,41 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerColor : MonoBehaviour
 {
-    private Color colorX;
-    private Color colorX_;
-    private Color colorY;
-    private Color colorY_;
-    private Color colorZ;
-    private Color colorZ_;
-
     public string rightColor;
     public string leftColor;
     public string frontColor;
     public string backColor;
     public string upColor;
     public string downColor;
+
+    public float right;
+    public float left;
+    public float front;
+    public float back; 
+    public float up;
+    public float down;
+
+    private Floor floor;
+
+    private Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
-        colorX = transform.GetChild(0).gameObject.GetComponent<Color>();
-        colorX_ = transform.GetChild(1).gameObject.GetComponent<Color>();
-        colorY = transform.GetChild(2).gameObject.GetComponent<Color>();
-        colorY_ = transform.GetChild(3).gameObject.GetComponent<Color>();
-        colorZ = transform.GetChild(4).gameObject.GetComponent<Color>();
-        colorZ_ = transform.GetChild(5).gameObject.GetComponent<Color>();
+        GameObject floorObj = GameObject.Find("Floor");
+        floor = floorObj.GetComponent<Floor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rightColor = colorX.color;
-        leftColor = colorX_.color;
-        frontColor = colorZ.color;
-        backColor = colorZ_.color;
-        upColor = colorY.color;
-        downColor = colorY_.color;
+        pos = transform.position;
+
+        down = floor.floor[Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.z)];
+        right = floor.floor[Mathf.RoundToInt(pos.x) + 1, Mathf.RoundToInt(pos.z)];
+        left = floor.floor[Mathf.RoundToInt(pos.x) - 1, Mathf.RoundToInt(pos.z)];
+        front = floor.floor[Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.z) + 1];
+        back = floor.floor[Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.z) - 1];
+
+        rightColor = GetColor(right);
+        leftColor = GetColor(left);
+        frontColor = GetColor(front);
+        backColor = GetColor(back);
+        downColor = GetColor(down);
+    }
+
+    private string GetColor(float floor)
+    {
+        string color = "NoFloor";
+        if(floor == 1)
+        {
+            color = "Red";
+        }
+
+        if(floor == 2)
+        {
+            color = "Blue";
+        }
+
+        if(floor == 3)
+        {
+            color = "Yellow";
+        }
+
+        if(floor == 4)
+        {
+            color = "White";
+        }
+
+        return color;
     }
 }
